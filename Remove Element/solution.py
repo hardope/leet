b1e3a7368f2691776c4fc3d57a6f03e6f2a7a01e
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-09 22:16:14 +0100

    Create solution.py

---
+++ Remove Element/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        while True:
+            try:
+                nums.pop(nums.index(val))
+            except:
+                break
